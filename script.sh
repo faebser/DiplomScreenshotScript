098f013283ebffab9dc4730517f95229a2409328
Fabian Frei <fabian.frei@esf-frei.ch>
2012-06-06 17:14:50 +0200

    script working now

--- script.sh
+++ script.sh
@@ -10,27 +10,24 @@ do
 	dir=`echo $file | cut -d "_" -f1`
 
 	if [ -d "$dir" ]; then
-	    	#Control will enter here if $DIRECTORY exists.
 		echo "converting $file"
-		#convert  "$file" -resize x540 "$dir/$file.png"
+		convert  "$file" -resize x540 "$dir/$file.png"
 	else
 		mkdir "$dir"
 		echo "converting $file"
-		#convert  "$file" -resize x540 "$dir/$file.png"
+		convert  "$file" -resize x540 "$dir/$file.png"
 	fi
 done
-#make gifs and slices
+#make gifs and stripes
 ls -d */ | while read -r dir
 do
 	realDir=`echo "$dir" | tr -d '/'`
-	#echo "$realDir"
 	echo "making gif for $realDir" 
 	convert -delay 100 -dispose Background "$dir"*.png -loop 0 "$realDir"/"$realDir".gif
-	#echo `ls "$dir"*.png`
 	echo "making vertical strip for $realDir"
-	convert "$dir"*.png -crop 10x0+480+0 +append "$realDir"/strip-vertical.png
+	convert "$realDir"/"$realDir"*.png -crop 10x0+480+0 +append "$realDir"/strip-vertical.png
 	echo "making horizontal strip for $realDir"
-	convert "$dir"*.png -crop 0x10+0+270 +append "$realDir"/strip-horizontal.png
+	convert "$realDir"/"$realDir"*.png -crop 0x10+0+270 -append "$realDir"/strip-horizontal.png
 done
 echo "done"
 
