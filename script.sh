b5e268b020b6e837842bb94eaaa57e8c43cc794e
Fabian Frei <fabian.frei@esf-frei.ch>
2012-06-06 16:38:31 +0200

    first commit, script working but not fully tested

---
+++ script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#replace all spaces with underscores
+ls *.bmp | while read -r FILE
+do
+    mv -v "$FILE" `echo $FILE | tr ' ' '_' `
+done
+#convert files to right directory
+ls *.bmp | while read -r file
+do 
+	dir=`echo $file | cut -d "_" -f1`
+
+	if [ -d "$dir" ]; then
+	    	#Control will enter here if $DIRECTORY exists.
+		echo "converting $file"
+		#convert  "$file" -resize x540 "$dir/$file.png"
+	else
+		mkdir "$dir"
+		echo "converting $file"
+		#convert  "$file" -resize x540 "$dir/$file.png"
+	fi
+done
+#make gifs and slices
+ls -d */ | while read -r dir
+do
+	realDir=`echo "$dir" | tr -d '/'`
+	#echo "$realDir"
+	echo "making gif for $realDir" 
+	convert -delay 100 -dispose Background "$dir"*.png -loop 0 "$realDir"/"$realDir".gif
+	#echo `ls "$dir"*.png`
+	echo "making vertical strip for $realDir"
+	convert "$dir"*.png -crop 10x0+480+0 +append "$realDir"/strip-vertical.png
+	echo "making horizontal strip for $realDir"
+	convert "$dir"*.png -crop 0x10+0+270 +append "$realDir"/strip-horizontal.png
+done
+echo "done"
+
